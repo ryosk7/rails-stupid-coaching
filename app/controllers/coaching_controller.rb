e0d1e9f5080e56aa28960bbf6f64c7aad77fc26b
Ryosuke Uchida <uchiryo7@gmail.com>
2017-04-03 15:01:39 +0900

    I create controller and html.erb.

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,7 +1,22 @@
 class CoachingController < ApplicationController
   def answer
+
+    @query = params[:query]
+    # puts "#{coach_answer_enhanced(your_message)}#{coach_answer(your_message)}"
+    if @query
+      if @query == "I am going to work right now!"
+        @answer = ""
+      elsif @query == @query.upcase
+        @answer = "I can feel your motivation!"
+      elsif @query[-1] == "?"
+        @answer = "Silly question! Get back to work."
+      elsif @query
+        @answer = "I don't care! Get back to work."
+      end
+    end
   end
 
   def ask
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'coaching/answer'
+  # get 'coaching/answer' # Automatically routes to coaching#answer
+  get '/answer', to: 'coaching#answer'
 
-  get 'coaching/ask'
+  post '/answer', to: 'coaching#answer'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get '/ask', to: 'coaching#ask'
+
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    end
+    ''
